2e1271c1d03631bb5076238728de4b02f01b034d
Myv-hs <matthewcoyle@me.com>
2018-01-23 17:17:49 +0100

    TP1exo01 sustr div optimisation 8+9 mv
    exo01.11

--- TP1/exo01.cpp
+++ TP1/exo01.cpp
@@ -6,6 +6,33 @@ struct fraction {
 	int n,d;
 };
 
+struct nbmixite {
+	int signe;
+	int ent;
+	fraction frac;
+};
+
+//8
+int pgcd (int a, int b){
+	if(a<0) return pgcd(-a,b);
+	if(b<0) return pgcd(a,-b);
+	if(a<b) return pgcd(b,a);
+	if(b==0) return a;
+	return pgcd(b,a%b);
+}
+
+//9
+fraction simpl (fraction a) {
+	fraction out;
+	int p = pgcd(a.n,a.d);
+	out.n = a.n/p; out.d = a.d/p;
+	if(out.d<0) {
+		out.d *=-1;
+		out.m *=-1;
+	}
+	return out;
+}
+
 //2
 void saisie (fraction &a){
 	cout << "gimme 2 numbers"; cin >> a.n >> a.d;
@@ -20,7 +47,7 @@ void affichage (fraction a) {
 fraction mult (fraction a, fraction b) {
 	fraction out;
 	out.n = a.n*b.n; out.d = b.d*b.d;
-	return out;
+	return simple(out);
 }
 
 //5
@@ -29,7 +56,7 @@ fraction add (fraction a, fraction b) {
 	temp.n = a.n*b.d; temp.d = a.d*b.d;
 	out.n = b.n*a.d; out.d = b.d*a.d;
 	out.n += temp.n;
-	return out;
+	return simpl(out);
 }
 
 //6
@@ -47,33 +74,34 @@ fraction inv (fraction a) {
 
 //7
 fraction sustr (fraction a, fraction b) {
-	fraction temp = inv(b);
-	return add(a,temp);
+	return add(a,inv(b));
 }
 
 fraction div (fraction a, fraction b) {
-	fraction temp;
-	temp = inv(b);
-	return mult(a,temp);
+	return mult(a,inv(b));
 }
 
-//8
-int pgcd (int a, int b){
-	if(a<0) return pgcd(-a,b);
-	if(b<0) return pgcd(a,-b);
-	if(a<b) return pgcd(b,a);
-	if(b==0) return a;
-	return pgcd(b,a%b);
-}
+//11
+nbmixite mixte (fraction a) {
+	nbmixite out;
 
-//9
-fraction simpl (fraction a) {
-	fraction out;
-	int p = pgcd(a.n,a.d);
-	out.n = a.n/p; out.d = a.d/p;
+	out.frac = a;
+
+	if(a.n<0 || a.d<0) out.signe = -1;
+	else out.signe = 1;
+
+	out.ent = a.n/a.d;
+
+	if(out.ent>0) out.frac.n = a.n%a.d;
+
 	return out;
+}
+
+nbmixite addM (nbmixite a, nbmixite b) {
+
 }
 
 int main () {
 
+	return 0;
 }
