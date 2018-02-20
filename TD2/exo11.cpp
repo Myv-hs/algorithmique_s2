6cd10c68a0394189adbf0619a33d557591e0d014
Myv-hs <matthewcoyle@me.com>
2018-02-20 12:11:15 +0100

    TD2exo11c

--- TD2/exo11.cpp
+++ TD2/exo11.cpp
@@ -23,11 +23,23 @@ int tablo_int_max_index (tablo_int T, int t, int mx){
 	return idx;
 }
 
-int plus_petit_indice_du_max_rec (tablo_int T, int n){
+int plus_petit_indice_du_max_rec (tablo_int T, int n){ //Prof
 	if(n==1) return 0;
 	int ppi = plus_petit_indice_du_max_rec(T,n-1);
 	if(T[n-1]>T[ppi]) return n-1;
 	return ppi;
+}
+
+//c
+void tablo_int_maxtri (tablo_int &T, int t){
+	if(t<=0) return;
+	int minimax = plus_petit_indice_du_max_rec(T,t);
+	if(minimax != t-1) {
+		int swp = T[t-1];
+		T[t-1] = T[minimax];
+		T[minimax] = swp;
+	}
+	tablo_int_maxtri(T,t-1);
 }
 
 int main(){
@@ -35,6 +47,9 @@ int main(){
 	std::cout << tablo_int_max(A,6) <<std::endl;
 	std::cout << tablo_int_max_index(A,6,tablo_int_max(A,6)) <<std::endl;
 	std::cout << plus_petit_indice_du_max_rec(A,6) <<std::endl;
+	tablo_int_maxtri(A,6);
+	std::cout << plus_petit_indice_du_max_rec(A,6) <<std::endl;
+
 
 	return 0;
 }
