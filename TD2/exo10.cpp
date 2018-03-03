56762ad1f733784d0b52f2817952d064e6edfe1a
Myv-hs <matthewcoyle@me.com>
2018-03-03 22:14:48 +0100

    TD2 exo10

---
+++ TD2/exo10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// Prototypes
+bool paire (int a);
+bool impaire (int a);
+
+bool paire (int a){
+	if(a==0) return 1;
+	if(a==1) return 0;
+	impaire(a-1);
+}
+
+bool impaire (int a){
+	if(a==0) return 0;
+	if(a==1) return 1;
+	paire(a-1);
+}
+
+int main (){
+	int a; std::cin >>a;
+	if(paire(a)) std::cout <<a<<" est paire\n";
+	return 0;
+}
