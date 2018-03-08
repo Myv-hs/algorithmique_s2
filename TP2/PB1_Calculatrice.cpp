ee82d4148e9284bc62143c24055dc12020a6c56f
Myv-hs <matthewcoyle@me.com>
2018-03-08 19:36:43 +0100

    PB1 prototypes

---
+++ TP2/PB1_Calculatrice.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+string extrait (string e, int pos, int lg);
+bool estsigne (char c);
+bool estchiffre (char c);
+int parent_ouvertes (string e);
+int cherchesigne (string e, int pos);
+int num (char c);
+int convertit (string n);
+int calcule (int g, char signe, int d);
+int evalue (string e);
+bool valide (string e);
+void saisie ();
+
+int main () {
+	return 0;
+}
