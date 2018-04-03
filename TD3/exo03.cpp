69c09e33c206ad367c6a75be6a870f39f2973b96
Myv-hs <matthewcoyle@me.com>
2018-04-03 12:15:31 +0200

    TD3exo3 dgree, pow, pow_it(prof), valeurEn

--- TD3/exo03.cpp
+++ TD3/exo03.cpp
@@ -15,3 +15,28 @@ void insererEnTete (polynome &P, monome m) {
 	P = p;
 }
 
+int degre (polynome P) {
+	if(P==NULL) return 0;
+	if(P->suiv == NULL) return P->a.pow;
+	int nxt = degre(P->suiv);
+	if(nxt < P->a.pow) return P->a.pow;
+	return nxt;
+}
+
+float pow (float x, int p){
+	if(p==0) return 1;
+	return x*pow(x,p-1);
+}
+
+float pow_it(float x, int p){ //prof
+	float out = 1;
+	for(int i=0;i<=p;i++){
+		out = out * x;
+	}
+	return out;
+}
+
+float valeurEn (polynome P, float x) {
+	if(P==NULL) return 0;
+	return P->a.coeff*pow(x, P->a.pow)+valeurEn(P->suiv, x);
+}
