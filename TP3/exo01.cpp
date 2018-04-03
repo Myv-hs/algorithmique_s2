07e26de154332f24ab56d11831836a10479bf456
Myv-hs <matthewcoyle@me.com>
2018-04-03 11:31:28 +0200

    TP3 exo01 fix

--- TP3/exo01.cpp
+++ TP3/exo01.cpp
@@ -29,7 +29,7 @@ void insrt_sort (T &a, int b){
 	if(b>0){
 		std::cout <<b<<">0\n";
 		int p = insrt_pos(a[b-1],a,b);
-		std::cout <<p<<" is the index to insert at\n";
+		std::cout <<p<<" is the index to insert "<<a[b-1]<<" at\n";
 		if(p==b-1) insrt_sort(a,b-1);
 		else {
 			rec_add(a[b-1],p,a,b);
@@ -39,9 +39,9 @@ void insrt_sort (T &a, int b){
 }
 
 int main(){
-	T a = {0,1,3,5};
+	T a = {1,2,3,5};
 	T t = {1,4,2,3};
-	std::cout << insrt_pos(2,a,4) <<std::endl;
+	std::cout << insrt_pos(0,a,4) <<std::endl;
 	rec_add(2,insrt_pos(2,a,4),a,4);
 	insrt_sort(t,4);
 	for(int i=0;i<4;i++){
